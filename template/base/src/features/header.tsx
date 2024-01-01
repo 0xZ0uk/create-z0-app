e561bb1fecc2f1e22c37c3331e540dedcb0497f3
Pedro Santana <pedrosantana@duck.com>
2024-01-01 17:45:27 +0000

    Add Prisma ORM

--- template/base/src/features/header.tsx
+++ template/base/src/features/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { CommandDialog, CommandInput } from "@/components/ui/command";
 import {
   NavigationMenu,
   NavigationMenuItem,
